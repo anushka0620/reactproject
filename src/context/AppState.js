f7336bfc560ce99442431ed8a4169dd3027f76ac
anushka0620 <anushkam613@gmail.com>
2022-09-01 12:55:21 +0530

    more exmaple of context

--- src/context/AppState.js
+++ src/context/AppState.js
@@ -1,17 +1,29 @@
-import React from "react";
+// React, { useState } from "react";
+// AppContext from "./AppContext";
+import { useState } from "react";
 import AppContext from "./AppContext";
-
 export default function AppState(props) {
-  const [store, setStore] = React.useState({
-    name: "Anushka Mishra",
-    address: "Lucknow",
-  });
-
-  const logSomething = ()=>{
-    console.log("Login here");
-  }
-
-  return <AppContext.Provider value={{store,logSomething}}>
+  const [name, setNAme] = useState("ETP Batch")
+  return <AppContext.Provider value={{name,setNAme}}>
      {props.children}
   </AppContext.Provider>;
 }
+//export default function AppState(props) {
+  //const [store, setStore] = React.useState({
+    //name: "Anushka Mishra",
+    //address: "Lucknow",
+  //});
+
+  //const logSomething = ()=>{
+ //   console.log("Login here");
+ // }
+
+//const [name,setName] = useState"Etp batch"
+  //return 
+  //<AppContext.Provider value ={{name,setName}}>
+    // {props.children}
+  //</AppContext.Provider>
+
+  //<AppContext.Provider value={{store,logSomething}}>
+    // {props.children}
+  //</AppContext.Provider>;
